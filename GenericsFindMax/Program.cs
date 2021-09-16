7f23639dedab4f15a61854ecbc49b14a52f5b0d0
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-16 17:09:56 +0530

    Welcome Message added

---
+++ GenericsFindMax/Program.cs
@@ -0,0 +1,12 @@
+﻿ using System;
+
+namespace GenericsFindMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generics Programs");
+        }
+    }
+}
