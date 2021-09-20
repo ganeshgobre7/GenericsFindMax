03ef0f811d50d28fdb8657893e89e919ae01d7c5
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-20 14:09:19 +0530

    Generic method used to find max number

--- GenericsFindMax/FindMaxNumber.cs
+++ GenericsFindMax/FindMaxNumber.cs
@@ -6,7 +6,7 @@ namespace GenericsFindMax
 {
     class FindMaxNumber
     {
-        public static int IntMaxNumber(int value1, int value2, int value3)
+        public static dynamic MaxNumber<datatype>(dynamic value1, dynamic value2, dynamic value3) where datatype : IComparable
         {
             if (value1.CompareTo(value2) > 0 && value1.CompareTo(value3) > 0)
             {
@@ -21,39 +21,6 @@ namespace GenericsFindMax
                 return value3;
             }
             return value1;
-        }
-
-        public static float FloatMaxNumber(float value1, float value2, float value3)
-        {
-            if (value1.CompareTo(value2) > 0 && value1.CompareTo(value3) > 0)
-            {
-                return value1;
-            }
-            if (value2.CompareTo(value1) > 0 && value2.CompareTo(value3) > 0)
-            {
-                return value2;
-            }
-            if (value3.CompareTo(value1) > 0 && value3.CompareTo(value2) > 0)
-            {
-                return value3;
-            }
-            return value1;
-        }
-        public static string StringMaxNumber(string value1, string value2, string value3)
-        {
-            if (value1.CompareTo(value2) > 0 && value1.CompareTo(value3) > 0)
-            {
-                return value1;
-            }
-            if (value2.CompareTo(value1) > 0 && value2.CompareTo(value3) > 0)
-            {
-                return value2;
-            }
-            if (value3.CompareTo(value1) > 0 && value3.CompareTo(value2) > 0)
-            {
-                return value3;
-            }
-            return value1;
-        }
+        }       
     }
 }
--- GenericsFindMax/Program.cs
+++ GenericsFindMax/Program.cs
@@ -7,13 +7,11 @@ namespace GenericsFindMax
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Generics Programs");
-            int intMax = FindMaxNumber.IntMaxNumber(100, 200, 300);
+            int intMax = FindMaxNumber.MaxNumber<int>(100, 200, 300);
             Console.WriteLine("Maximum Integer Number is :" + intMax);
-
-            float flotMax = FindMaxNumber.FloatMaxNumber(100.10f, 200.20f, 300.30f);
-            Console.WriteLine("Maximum Float Number is :" + flotMax);
-
-            string stringMax = FindMaxNumber.StringMaxNumber("Apple", "Peach", "Banana");
+            float floatMax = FindMaxNumber.MaxNumber<float>(100.10f, 200.20f, 300.30f);
+            Console.WriteLine("Maximum Float Number is :" + floatMax);
+            string stringMax = FindMaxNumber.MaxNumber<string>("Apple", "Peach", "Banana");
             Console.WriteLine("Maximum String Number is :" + stringMax);
         }
     }   
